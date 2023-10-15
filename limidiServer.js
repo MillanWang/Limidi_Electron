f1d9e158da20639afada79a19ef44fde4e30ddc1
Millan Wang <real.millan.wang@gmail.com>
2023-10-15 17:44:51 -0400

    added environment based virtual vs attach to loopmidi system

--- limidiServer.js
+++ limidiServer.js
@@ -1,58 +1,71 @@
 const express = require("express");
-const { Output } = require('easymidi');
+const { Output, Input, getInputs, getOutputs } = require("easymidi");
+
+const isWindows = process.platform === "win32";
 
 const app = express();
-
-let virtualOutput;
+let server;
+let midiOutput;
 function startLimidiServer(port) {
-    app.listen(port, "0.0.0.0", () => {
-        virtualOutput = new Output('Limidi', true);
-    })
+    server = app.listen(port, "0.0.0.0", () => {
+        if (isWindows) {
+            // On windows, connect to loopmidi - FOR NOW UNTIL TeVirtualMidi can be integrated and lisenced with proper installation
+            try {
+                const loopMidiInputName = "mill";
+                midiOutput = new Output(loopMidiInputName);
+            } catch (e) {
+                console.log("input name not found");
+                /* TODO
+                    - Make a system that allows device configuration. 
+                    - It'll need to have a permanent selector in the GUI to address dynamic device switching when disconnected
+                */
+            }
+        } else {
+            // Mac - Create custom virtual input
+            midiOutput = new Output("Limidi", true); //
+        }
+    });
 
     setupEndpoints();
 }
 
 function setupEndpoints() {
     app.get("/Heartbeat", (req, res) => {
-        res.send(`Heartbeat at:  ${Date.now()}`)
+        res.send(`Heartbeat at:  ${Date.now()}`);
+        closeLimidiServer();
     });
 
     app.get("/MidiNote", (req, res) => {
-        sendMidiNote(
-            (req.query.isNoteOn === "true") ? true : false,
-            req.query.noteNumber ?? 0,
-            req.query.velocity ?? 0
-        )
-        res.send("MidiNote: " + Date.now() + JSON.stringify(req.query))
+        sendMidiNote(req.query.isNoteOn === "true" ? true : false, req.query.noteNumber ?? 0, req.query.velocity ?? 0);
+        res.send("MidiNote: " + Date.now() + JSON.stringify(req.query));
     });
 
     app.get("/ControlChangeInput", (req, res) => {
-        sendControlChange(
-            req.query.controlIndex ?? 0,
-            req.query.level ?? 0,
-        )
-        res.send("ControlChangeInput: " + Date.now() + JSON.stringify(req.query))
+        sendControlChange(req.query.controlIndex ?? 0, req.query.level ?? 0);
+        res.send("ControlChangeInput: " + Date.now() + JSON.stringify(req.query));
     });
 }
 
-
-
-function sendMidiNote(isNoteOn, noteNumber, velocity,) {
-    virtualOutput.send(isNoteOn ? "noteon" : "noteoff", {
-        note: noteNumber,
-        velocity: isNoteOn ? velocity : 0,
-        channel: 0
-    })
+function closeLimidiServer() {
+    server.close();
 }
 
-function sendControlChange(controlIndex, level,) {
-    virtualOutput.send("cc", {
+function sendMidiNote(isNoteOn, noteNumber, velocity) {
+    midiOutput.send(isNoteOn ? "noteon" : "noteoff", {
+        note: noteNumber,
+        velocity: isNoteOn ? velocity : 0,
+        channel: 0,
+    });
+}
+
+function sendControlChange(controlIndex, level) {
+    midiOutput.send("cc", {
         controller: controlIndex,
         value: level,
-        channel: 0
-    })
+        channel: 0,
+    });
 }
 
 module.exports = {
-    startLimidiServer
-}
+    startLimidiServer,
+};
--- main.js
+++ main.js
@@ -4,8 +4,10 @@ const path = require("node:path");
 const createWindow = () => {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        maxWidth: 800,
+        maxHeight: 600,
+        minWidth: 400,
+        minHeight: 400,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
