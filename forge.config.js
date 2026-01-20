f0132d2ea96864bc5d0bc57f424b502c399b2ad4
Millan Wang <real.millan.wang@gmail.com>
2026-01-19 21:03:17 -0500

    add make and related commands for build

--- forge.config.js
+++ forge.config.js
@@ -1,12 +1,19 @@
 module.exports = {
   packagerConfig: {
     asar: true,
+    name: "Limidi",
+    executableName: "limidi",
+    appBundleId: "com.limidi.electron",
+    appVersion: "1.0.0",
+    icon: undefined, // Add path to .ico/.icns/.png icon if you have one
   },
   rebuildConfig: {},
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
-      config: {},
+      config: {
+        name: "limidi",
+      },
     },
     {
       name: "@electron-forge/maker-zip",
@@ -14,11 +21,21 @@ module.exports = {
     },
     {
       name: "@electron-forge/maker-deb",
-      config: {},
+      config: {
+        options: {
+          maintainer: "Millan Wang",
+          homepage: "https://github.com/yourusername/limidi-electron",
+        },
+      },
     },
     {
       name: "@electron-forge/maker-rpm",
-      config: {},
+      config: {
+        options: {
+          maintainer: "Millan Wang",
+          homepage: "https://github.com/yourusername/limidi-electron",
+        },
+      },
     },
   ],
   plugins: [
